6a9b986482ac76c29a57104a268d6e9253aeca8a
AliMirzaei <Alimirzaeii@hotmail.com>
2023-02-23 12:36:27 +0330

    Add components

--- lib/screens/book_detail_screen.dart
+++ lib/screens/book_detail_screen.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/material.dart' ;
+import 'package:unimics_flutter/constants/colors.dart';
+import 'package:unimics_flutter/constants/sizes.dart';
+import '../components/icon.dart';
 
 class BookDetailScreen extends StatelessWidget{
+  const BookDetailScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.background,
       appBar: AppBar(
-        title: Text("Second Page"),
+        backgroundColor: AppColors.background,
+        elevation: 0,
+        leading: MyIcon(
+            icon: Icons.arrow_back_ios_new,
+            onTap: () => Navigator.of(context).pop(),
+        ),
+
+        actions: [
+           MyIcon(
+            icon: Icons.bookmark,
+            onTap: () {},
+          ),
+          const SizedBox(width: AppSizes.padding),
+           MyIcon(
+            icon: Icons.more_vert,
+            onTap: () {},
+          ),
+          const SizedBox(width: AppSizes.padding,)
+        ],
       ),
     );
   }
